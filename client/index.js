3ed77831c744743aee7025a8cfefab4ca0dae323
Garrett Welson <garrettwelson@gmail.com>
2019-07-03 17:13:54 -0500

    test functionality on new branch

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Hello World, from Garrett 🙏!</div>, document.getElementById('app'));
+ReactDOM.render(<div>Hello World, from Garrett 💯🙏💯!</div>, document.getElementById('app'));
