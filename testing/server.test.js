55d4edc3a929b2325c03a7d51de3e628bdafd663
Garrett Welson <garrettwelson@gmail.com>
2019-07-09 15:49:01 -0500

    Add sum.js to demonstrate basic jest functionality and provide target for testing

--- testing/server.test.js
+++ testing/server.test.js
@@ -1,2 +1,6 @@
-const server = require('../server/server');
+//const server = require('../server/server');
+const sum = require('./sum');
 
+test('adds 1+2 and returns 3', () => {
+  expect(sum(1,2)).toBe(3);
+});
