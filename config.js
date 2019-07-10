ab08c30bb7f99102b78e20b86a307fc0c8fc0bfb
Garrett Welson <garrettwelson@gmail.com>
2019-07-10 13:30:49 -0500

    Clean up to-do message

--- config.js
+++ config.js
@@ -5,8 +5,3 @@ module.exports = {
   MONGO_PASSWORD: 'EGfo33EkJkyOST4G'
 };
 
-/*
-TODO: 
-run eb setenv 'mongodb+srv://garrettwelson:EGfo33EkJkyOST4G@hackmazon-navbar-c54z1.mongodb.net/test?retryWrites=true&w=majority'
-refactor code to account for URL in PROCESS.ENV.WHATEVERICALLIT
-*/
