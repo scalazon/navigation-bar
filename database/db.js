19270be0a472a777965293f12c687257b8062513
Garrett Welson <garrettwelson@gmail.com>
2019-07-10 18:41:38 -0500

    Remove reference to config.js to facilitate deployment

--- database/db.js
+++ database/db.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require('mongodb');
-const { MONGO_USER, MONGO_PASSWORD} = require('../config');
+const MONGO_USER = process.env.MONGO_USER;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 
 const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@hackmazon-navbar-c54z1.mongodb.net/test?retryWrites=true&w=majority`;
 const options = {
--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const app = require('./app');
-const { PORT, HOST} = require('../config');
 
-const port = PORT || 3000;
-const host = HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || '0.0.0.0';
 
 app.listen(port, host, () => {
   console.log(`Navbar server running http://${host}:${port}`);
