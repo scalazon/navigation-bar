1828896992d61543db35d5b639764ca9eb1363e3
Garrett Welson <garrettwelson@gmail.com>
2019-07-16 15:42:39 -0500

    Refactor DB to use Mongoose, eliminate multiple connection issue, refactor data flow in Search react component to be more efficient

--- database/index.js
+++ database/index.js
@@ -1,16 +1,7 @@
-const { MongoClient } = require('mongodb');
 const MONGO_USER = process.env.MONGO_USER;
 const MONGO_PASSWORD = process.env.MONGO_PASS;
 const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0-c54z1.mongodb.net/test?retryWrites=true&w=majority`
-const options = {
-  useNewUrlParser: true
-};
 
-function connect(uri) {
-  return MongoClient.connect(uri).then(client => client.db());
-}
+const mongoose = require('mongoose');
 
-module.exports = async function() {
-  let databases = await connect(uri)
-  return databases;
-}
+module.exports = mongoose.connect(uri, {useNewUrlParser:true, dbName:'products'});
