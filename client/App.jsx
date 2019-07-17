f328e47aca4229ca04d55d4389273d4cb01ac3d8
Garrett Welson <garrettwelson@gmail.com>
2019-07-16 20:41:36 -0500

    Implement basic structure of bottom navbar and start building style

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import SearchBar from './components/Search.jsx'
 import Navbar from 'react-bootstrap/Navbar'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import Nav from 'react-bootstrap/Nav'
+import {FiMapPin} from 'react-icons/fi'
+import { IconContext } from "react-icons";
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+
 class App extends React.Component {
 
   constructor(props) {
@@ -15,39 +19,53 @@ class App extends React.Component {
   
   
   render() {
-    
-    const imgStyle = {
-      maxHeight: '10%',
-      maxWidth: '15%'
+    let deliverToFont = {
+      fontSize: '12px',
+      lineHeight: '14px',
+      fontWeight: '400',
+      marginTop: '9px',
+      color: 'white'
+    };
+
+    let cityZipFont = {
+      fontSize: '14px',
+      lineHeight: '15px',
+      fontWeight: '700',
+      paddingBottom: '5px',
+      color: 'white'
     }
-    
     return (
       <>
-        <Navbar bg='dark' expand='xl'>
-          <Row>
-            <Col xl='auto'>
-              <Navbar.Brand href="#">
-                <img 
-                  src='https://elasticbeanstalk-us-east-2-746219401089.s3.us-east-2.amazonaws.com/Hackmazon-logo-white.png'
-                  width='150'
-                  height='45'
-                />
-              </Navbar.Brand>
-            </Col>
-            <Col xl='auto'>
-              <SearchBar />
-            </Col>
-            <Col>
-            </Col>
-          </Row>
-          {/* <Row>
+        <Navbar bg='dark'> 
+          <Navbar.Brand href="#">
+            <img 
+              src='https://elasticbeanstalk-us-east-2-746219401089.s3.us-east-2.amazonaws.com/Hackmazon-logo-white.png'
+              width='150'
+              height='45'
+            />
+          </Navbar.Brand>  
+          <SearchBar />
+        </Navbar>
+        <Navbar bg='dark' variant='dark'>
+          <Navbar.Brand href='#'>
+            <IconContext.Provider value={{ color: "white", size: '1.2em'}}>
+              <FiMapPin />
+            </IconContext.Provider> 
+          </Navbar.Brand>
+          <Nav variant='dark'>
+            <Nav.Link href='#'>
+              <Container>
+                <Row><span style={deliverToFont}>Deliver to Garrett</span></Row>
+                <Row><span style={cityZipFont}>Austin 78703</span></Row>
+              </Container>
+            </Nav.Link>
             <Nav.Link href="#">Deliver to Austin</Nav.Link>
             <Nav.Link href="#">Today's Deals</Nav.Link>
             <Nav.Link href="#">My Hackmazon.com</Nav.Link>
             <Nav.Link href="#">Buy Again</Nav.Link>
             <Nav.Link href="#">Gift Cards</Nav.Link>
             <Nav.Link href="#">Help</Nav.Link>
-          </Row> */}
+          </Nav>
         </Navbar>
       </>
     )
