d11b5f7219d3deacf7c5859263a1c1956a609c57
Hadley Crowl <hadley.a.crowl@gmail.com>
2019-07-25 16:30:27 -0500

    make db default to 127.0.0.1 if no env is present. Added mongo services in travis config

--- database/index.js
+++ database/index.js
@@ -1,6 +1,13 @@
 const MONGO_USER = process.env.MONGO_USER;
 const MONGO_PASSWORD = process.env.MONGO_PASS;
-const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0-c54z1.mongodb.net/test?retryWrites=true&w=majority`
+
+//by default, set the uri to the travis test route
+const uri = `127.0.0.1`
+
+//if we have data from an env file, create an atlas uri with that
+if (MONGO_USER && MONGO_PASSWORD) {
+  uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0-c54z1.mongodb.net/test?retryWrites=true&w=majority`
+}
 
 const mongoose = require('mongoose');
 
